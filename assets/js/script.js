9f20a0a476f4b46f3ee68457d3cf4f7c19e6ebc4
elenagurchinskaia <136741210+elenagurchinskaia@users.noreply.github.com>
2023-07-16 23:19:51 -0500

    search city function

--- assets/js/script.js
+++ assets/js/script.js
@@ -36,34 +36,34 @@ fetch('https://test.api.amadeus.com/v1/shopping/flight-destinations?origin=PAR&m
 
 // ----------- THIS IS OUR API INFORMATION FROM SPOTIFY-------------//
 
-var client_id = "a4c70f559e3e46df90bb4d9dc0dd39f4";
-var client_secret = "70dbab28cfff477c98d85db71a635987";
+// var client_id = "a4c70f559e3e46df90bb4d9dc0dd39f4";
+// var client_secret = "70dbab28cfff477c98d85db71a635987";
 
-var authOptions = {
-  url: "https://accounts.spotify.com/api/token",
-  headers: {
-    Authorization:
-      "Basic " +
-      new Buffer.from(client_id + ":" + client_secret).toString("base64"),
-  },
-  form: {
-    grant_type: "client_credentials",
-  },
-  json: true,
-};
+// var authOptions = {
+//   url: "https://accounts.spotify.com/api/token",
+//   headers: {
+//     Authorization:
+//       "Basic " +
+//       new Buffer.from(client_id + ":" + client_secret).toString("base64"),
+//   },
+//   form: {
+//     grant_type: "client_credentials",
+//   },
+//   json: true,
+// };
 
-request.post(authOptions, function (error, response, body) {
-  if (!error && response.statusCode === 200) {
-    var token = body.access_token;
-  }
-});
+// request.post(authOptions, function (error, response, body) {
+//   if (!error && response.statusCode === 200) {
+//     var token = body.access_token;
+//   }
+// });
 
 // ----------- THIS IS OUR PSEUDOCODE-------------//
 
 //user searches for a city
-localStorage.setItem(city);
-localStorage.setItem(lat);
-localStorage.setItem(lon);
+// localStorage.setItem(city);
+// localStorage.setItem(lat);
+// localStorage.setItem(lon);
 //city search api - amadeus
 //use data.length to create # of elements(buttons)
 //get the value of data[i].name + ", " + data[i].stateCode + ", " + data[i].countryCode
@@ -72,6 +72,50 @@ localStorage.setItem(lon);
 //use localStorage to setItem for the coordinates variable
 //redirect the user to the city-info.html
 
-var { lat, lon } = data.coord;
-var cityInput = document.querySelector("#cityName");
-var city = cityInput.value.trim();
+// var { lat, lon } = data.coord;
+// var city = cityInputEl.value.trim();
+
+var cityInputEl = document.querySelector("#cityName");
+var searchBtn = document.querySelector(".search-btn");
+
+varAPIKey = "";
+
+var getCityCoordinates = function (event) {
+  event.preventDefault();
+  var city = cityInputEl.value.trim();
+  if (city) {
+    search(city);
+    storeSearch(city);
+    cityInputEl.value = "";
+  }
+};
+
+var searchCity = function (cityInfo) {
+  var apiUrl =
+    "https://test.api.amadeus.com/v1/reference-data/locations/cities?keyword=" +
+    city;
+
+  fetch(apiUrl).then(function (response) {
+    if (response.ok) {
+      response.json().then(function (data) {
+        console.log(data);
+        return response.json();
+        // storeSearch(data.length, cityInfo);
+      });
+    }
+  });
+};
+
+// var storeSearch = function (coordinates,)
+
+// searchBtn.addEventListener("submit", function (event) {
+//   event.preventDefault();
+//   var city = userInput.value.trim();
+//   if (city) {
+//     search(city);
+//     storeSearch(city);
+//     userInput.value = "";
+//   }
+// });
+
+searchBtn.addEventListener("submit", getCityCoordinates);
