fafa219e3254a81624129ef5c4ba036508f30ca6
Lainey Creighton <dev.lainey@gmail.com>
2023-07-18 08:33:36 -0700

    add food options fetch

--- assets/js/city-info.js
+++ assets/js/city-info.js
@@ -12,42 +12,64 @@
 // -------------------------------------------- SAFETY STATS ---------------------------------------------------------//
 
 function safetyStats() {
-var myHeaders = new Headers();
-myHeaders.append("Authorization", "Bearer jYLjAG4DNvK0Co47bbRjSparZ6Ps");
+  var myHeaders = new Headers();
+  myHeaders.append("Authorization", "Bearer jYLjAG4DNvK0Co47bbRjSparZ6Ps");
 
-var requestOptions = {
-  method: 'GET',
-  headers: myHeaders,
-  redirect: 'follow'
-};
+  var requestOptions = {
+    method: 'GET',
+    headers: myHeaders,
+    redirect: 'follow'
+  };
 
 
-fetch("https://test.api.amadeus.com/v1/safety/safety-rated-locations?latitude=47.60621&longitude=-122.33207&radius=15&page%5Boffset%5D=0", requestOptions)
-  .then(response => response.text())
-  .then(result => console.log(result))
-  .catch(error => console.log('error', error));
+  fetch("https://test.api.amadeus.com/v1/safety/safety-rated-locations?latitude=47.60621&longitude=-122.33207&radius=15&page%5Boffset%5D=0", requestOptions)
+    .then(response => response.text())
+    .then(result => console.log(result))
+    .catch(error => console.log('error', error));
 };
 
 safetyStats();
 
-  // add if else conditional statements
+// add if else conditional statements
 
-  //   for (var i = 0; i <= data.length; i++) {}
+//   for (var i = 0; i <= data.length; i++) {}
 
-  //   loop thru the safety stats items
-  //  for (var i = 0; i < ...; i++) {
-  // var lgbtq = data[1].lgbtq
-  // var medical = data[1].medical
-  // var overall = data[1].overall
-  // vr physicalHarm = data[2].physicalHarm
-  // var politicalFreedom = data[2].politicalFreedom
-  // var theft = data[2].theft
-  // var women = data[2].women
-  // append
+//   loop thru the safety stats items
+//  for (var i = 0; i < ...; i++) {
+// var lgbtq = data[1].lgbtq
+// var medical = data[1].medical
+// var overall = data[1].overall
+// vr physicalHarm = data[2].physicalHarm
+// var politicalFreedom = data[2].politicalFreedom
+// var theft = data[2].theft
+// var women = data[2].women
+// append
 
 
-  // -------------------------------------------- FOOD ---------------------------------------------------------//
+// -------------------------------------------- FOOD ---------------------------------------------------------//
 
-  // -------------------------------------------- PLACES OF INTEREST ---------------------------------------------------------//
+function foodOptions() {
 
-  // -------------------------------------------- EXCURSIONS ---------------------------------------------------------//
+  var myHeaders = new Headers();
+  myHeaders.append("Authorization", "Bearer F1xbRAYFvOkgPOmU0hrwZRqedDQh");
+  
+  var requestOptions = {
+    method: 'GET',
+    headers: myHeaders,
+    redirect: 'follow'
+  };
+  
+  fetch("https://test.api.amadeus.com/v1/reference-data/locations/pois?latitude=41.397158&longitude=2.160873&radius=15&page%5Blimit%5D=4&page%5Boffset%5D=0&categories=RESTAURANT", requestOptions)
+    .then(response => response.text())
+    .then(result => console.log(result))
+    .catch(error => console.log('error', error));
+};
+
+// -------------------------------------------- PLACES OF INTEREST ---------------------------------------------------------//
+
+// -------------------------------------------- EXCURSIONS ---------------------------------------------------------//
+
+// ------------------------------------ CALLING THE FUNCTIONS ------------------------------------//
+
+safetyStats();
+foodOptions();
