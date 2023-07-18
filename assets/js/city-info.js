0dd34e5d26635d6d1cdff3302394a34fc188bec7
Lainey Creighton <dev.lainey@gmail.com>
2023-07-18 15:03:23 -0700

    add safety stats to city info page

--- assets/js/city-info.js
+++ assets/js/city-info.js
@@ -8,21 +8,21 @@ var lat = city.latitude;
 
 // -------------------------------------------- MAP ---------------------------------------------------------//
 
-// function cityMap() {
-//   var myHeaders = new Headers();
-//   myHeaders.append("Authorization", "Bearer eyJhbGciOiJSUzUxMiIsImN0eSI6IkpXVCIsImlzcyI6IkhFUkUiLCJhaWQiOiJVWHJmbWhtc3g4Nzh6eDVidFhRNyIsImlhdCI6MTY4OTY5ODE1MSwiZXhwIjoxNjg5Nzg0NTUxLCJraWQiOiJqMSJ9.ZXlKaGJHY2lPaUprYVhJaUxDSmxibU1pT2lKQk1qVTJRMEpETFVoVE5URXlJbjAuLnhyb3QzcE9QZ3R1Q3JEUWh1akZVV0EuOW5WU1pfazFqS0plTGpkM0dXZXVYS2dyNTNDRmJSN2FBSFJwSm5iTWtZMUg2QlJDODc3a2xlR1JZSUk3YmVSOXFEOGF2SVI5ZzNYOEw3alJMUXQxc0Vlck4wV2R2QWhKR2N5VmpNMS05ZmNfWjRaYnJpb3hOVk14QXc4UDdES1lXLW01NmhwVjl5S3daRlpZaWVCMjNmX3hMdzNVNUtsQmJmNXVRVEJzWk5OV1R6R2o2dGJYNzJ2S19YbFJhbnpVS0U1VzdNS2g1SHB5TUtaU0EtT3NOVXQ0cGR4bHI4UG5VZnpSbkhXUEQwcy5OS04yTURVSkVnbExsMTBRYTBqS2RaV3RRMnJXTGM2U0hpYnk5R09WSndn.AeRohg56OFGMdK8C84QokdrFpzy7MeT7bAv9P3KyKGuO50u7qX3flcdF7ntBuWm542Cft1Zz8tQZfAwpWJNANnBTU_OboZudvlpz2r_76PgUFpRpS2Org2PVh3LzFHswW-T2pyoXwLeIgqpOPVo9DU5GkriuxuiTVsAJcQPfiPid4avnVeI8cusXRmfo_rReNcVUedllNW-M_-6RXsQQQ5uwLdlYT-jrdJ-uLwJakjq7-pyXfZWJtn1BaU_2y09nPtQcMZ10s92uhHnZM1UVwHDsnjq3ZPhFi6YelSdPm2Iw_fjHxDmjB6oXMP_xS2XQTnLVw31TBZ29cO4LiuECFg");
-
-//   var requestOptions = {
-//     method: 'GET',
-//     headers: myHeaders,
-//     redirect: 'follow'
-//   };
-
-//   fetch("https://image.maps.ls.hereapi.com/mia/1.6/mapview?c=47.60621%2C-122.33207&z=12&apiKey=H6XyiCT0w1t9GgTjqhRXxDMrVj9h78ya3NuxlwM7XUs", requestOptions)
-//     .then(response => response.json())
-//     .then(result => console.log('Map: ', result))
-//     .catch(error => console.log('error', error));
-// };
+function cityMap() {
+  var myHeaders = new Headers();
+  myHeaders.append("Authorization", "Bearer eyJhbGciOiJSUzUxMiIsImN0eSI6IkpXVCIsImlzcyI6IkhFUkUiLCJhaWQiOiJVWHJmbWhtc3g4Nzh6eDVidFhRNyIsImlhdCI6MTY4OTY5ODE1MSwiZXhwIjoxNjg5Nzg0NTUxLCJraWQiOiJqMSJ9.ZXlKaGJHY2lPaUprYVhJaUxDSmxibU1pT2lKQk1qVTJRMEpETFVoVE5URXlJbjAuLnhyb3QzcE9QZ3R1Q3JEUWh1akZVV0EuOW5WU1pfazFqS0plTGpkM0dXZXVYS2dyNTNDRmJSN2FBSFJwSm5iTWtZMUg2QlJDODc3a2xlR1JZSUk3YmVSOXFEOGF2SVI5ZzNYOEw3alJMUXQxc0Vlck4wV2R2QWhKR2N5VmpNMS05ZmNfWjRaYnJpb3hOVk14QXc4UDdES1lXLW01NmhwVjl5S3daRlpZaWVCMjNmX3hMdzNVNUtsQmJmNXVRVEJzWk5OV1R6R2o2dGJYNzJ2S19YbFJhbnpVS0U1VzdNS2g1SHB5TUtaU0EtT3NOVXQ0cGR4bHI4UG5VZnpSbkhXUEQwcy5OS04yTURVSkVnbExsMTBRYTBqS2RaV3RRMnJXTGM2U0hpYnk5R09WSndn.AeRohg56OFGMdK8C84QokdrFpzy7MeT7bAv9P3KyKGuO50u7qX3flcdF7ntBuWm542Cft1Zz8tQZfAwpWJNANnBTU_OboZudvlpz2r_76PgUFpRpS2Org2PVh3LzFHswW-T2pyoXwLeIgqpOPVo9DU5GkriuxuiTVsAJcQPfiPid4avnVeI8cusXRmfo_rReNcVUedllNW-M_-6RXsQQQ5uwLdlYT-jrdJ-uLwJakjq7-pyXfZWJtn1BaU_2y09nPtQcMZ10s92uhHnZM1UVwHDsnjq3ZPhFi6YelSdPm2Iw_fjHxDmjB6oXMP_xS2XQTnLVw31TBZ29cO4LiuECFg");
+  
+  var requestOptions = {
+    method: 'GET',
+    headers: myHeaders,
+    redirect: 'follow'
+  };
+  
+  fetch("https://image.maps.ls.hereapi.com/mia/1.6/mapview?c=" + lat + "%2C" + lon + "&z=12&apiKey=H6XyiCT0w1t9GgTjqhRXxDMrVj9h78ya3NuxlwM7XUs", requestOptions)
+    .then(response => response.json())
+    .then(result => console.log(result))
+    .catch(error => console.log('error', error));
+};
 
 // -------------------------------------------- SAFETY STATS ---------------------------------------------------------//
 
@@ -37,7 +37,7 @@ function safetyStats() {
   };
 
 
-  fetch("https://test.api.amadeus.com/v1/safety/safety-rated-locations?latitude=47.606210&longitude=-122.332070&page%5Blimit%5D=1&page%5Boffset%5D=0", requestOptions)
+  fetch("https://test.api.amadeus.com/v1/safety/safety-rated-locations?latitude=" + lat + "&longitude=-" + lon + "&page%5Blimit%5D=1&page%5Boffset%5D=0", requestOptions)
     .then(response => response.json())
     .then(result => {
       console.log('Safety Stats: ');
@@ -66,34 +66,29 @@ function safetyStats() {
       console.log(lgbtqNum);
 
       // //----------MEDICAL SCORE----------//
-      var medicalScore = document.createElement('li');
+      var medicalScore = document.getElementById('medicalScore');
       medicalScore.textContent = medicalNum;
-      safetyContainer.appendChild(medicalScore);
       console.log(medicalNum);
 
-      // //----------HARM SCORE----------//
-      // var physicalHarmScore = document.createElement('li');
-      // physicalHarmScore.textContent = physicalHarmNum;
-      // safetyContainer.appendChild(physicalHarmScore);
-      // console.log(physicalHarmNum);
+      //----------HARM SCORE----------//
+      var physicalHarmScore = document.getElementById('harmScore');
+      physicalHarmScore.textContent = physicalHarmNum;
+      console.log(physicalHarmNum);
 
-      // //----------POLITICAL SCORE----------//
-      // var politicalFreedomScore = document.createElement('li');
-      // politicalFreedomScore.textContent = politicalFreedomNum;
-      // safetyContainer.appendChild(politicalFreedomScore);
-      // console.log(politicalFreedomNum);
+      //----------POLITICAL SCORE----------//
+      var politicalFreedomScore = document.getElementById('politicalScore');
+      politicalFreedomScore.textContent = politicalFreedomNum;
+      console.log(politicalFreedomNum);
 
-      // //----------THEFT SCORE----------//
-      // var theftScore = document.createElement('li');
-      // theftScore.textContent = theftNum;
-      // safetyContainer.appendChild(theftScore);
-      // console.log(theftNum);
+      //----------THEFT SCORE----------//
+      var theftScore = document.getElementById('theftScore');
+      theftScore.textContent = theftNum;
+      console.log(theftNum);
 
-      // //----------WOMEN SCORE----------//
-      // var womenScore = document.createElement('li');
-      // womenScore.textContent = womenNum;
-      // safetyContainer.appendChild(womenScore);
-      // console.log(womenNum);
+      //----------WOMEN SCORE----------//
+      var womenScore = document.createElement('womenScore');
+      womenScore.textContent = womenNum;
+      console.log(womenNum);
 
     })
     .catch(error => console.log('error', error));
@@ -112,7 +107,7 @@ function foodOptions() {
     redirect: 'follow'
   };
 
-  fetch("https://test.api.amadeus.com/v1/reference-data/locations/pois?latitude=41.397158&longitude=2.160873&radius=15&page%5Blimit%5D=4&page%5Boffset%5D=0&categories=RESTAURANT", requestOptions)
+  fetch("https://test.api.amadeus.com/v1/reference-data/locations/pois?latitude=" + lat + "&longitude=" + lon + "&radius=15&page%5Blimit%5D=4&page%5Boffset%5D=0&categories=RESTAURANT", requestOptions)
     .then(response => response.json())
     .then(result => console.log('Food: ', result))
     .catch(error => console.log('error', error));
@@ -131,7 +126,7 @@ function sightsOptions() {
     redirect: 'follow'
   };
 
-  fetch("https://test.api.amadeus.com/v1/reference-data/locations/pois?latitude=41.397158&longitude=2.160873&radius=15&page%5Blimit%5D=4&page%5Boffset%5D=0&categories=SIGHTS", requestOptions)
+  fetch("https://test.api.amadeus.com/v1/reference-data/locations/pois?latitude=" + lat + "&longitude=" + lon + "&radius=15&page%5Blimit%5D=4&page%5Boffset%5D=0&categories=SIGHTS", requestOptions)
     .then(response => response.json())
     .then(result => console.log('Places of Interest: ', result))
     .catch(error => console.log('error', error));
@@ -139,26 +134,26 @@ function sightsOptions() {
 
 // -------------------------------------------- EXCURSIONS ---------------------------------------------------------//
 
-// function toursOptions() {
+function toursOptions() {
 
-//   var myHeaders = new Headers();
-//   myHeaders.append("Authorization", "Bearer By6cK9Hh6iG3ATbvfojt9d3ZYqI1");
+  var myHeaders = new Headers();
+  myHeaders.append("Authorization", "Bearer By6cK9Hh6iG3ATbvfojt9d3ZYqI1");
 
-//   var requestOptions = {
-//     method: 'GET',
-//     headers: myHeaders,
-//     redirect: 'follow'
-//   };
+  var requestOptions = {
+    method: 'GET',
+    headers: myHeaders,
+    redirect: 'follow'
+  };
 
-//   fetch("https://test.api.amadeus.com/v1/shopping/activities?latitude=41.397158&longitude=2.160873&radius=15", requestOptions)
-//     .then(response => response.json())
-//     .then(result => console.log('Excursions: ', result))
-//     .catch(error => console.log('error', error));
-// }
+  fetch("https://test.api.amadeus.com/v1/shopping/activities?latitude=" + lat + "&longitude=" + lon + "&radius=15", requestOptions)
+    .then(response => response.json())
+    .then(result => console.log('Excursions: ', result))
+    .catch(error => console.log('error', error));
+}
 
 // ------------------------------------ CALLING THE FUNCTIONS ------------------------------------//
 
-// cityMap();
+cityMap();
 safetyStats();
 // foodOptions();
 // sightsOptions();
